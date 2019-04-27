49a2c94c4a8ef593db628ff45841f600210c9d13
nstfk <nohaseddik@hotmail.com>
2019-04-27 12:35:33 +0200

    Update run_classifier.py

--- run_classifier.py
+++ run_classifier.py
@@ -316,17 +316,17 @@ class RqeProcessor(DataProcessor):
   def get_train_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_csv(os.path.join(data_dir, "rqe_train.csv")), "train")
+        self._read_txt(os.path.join(data_dir, "rqe_train.txt")), "train")
 
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_csv(os.path.join(data_dir, "rqe_dev.csv")), "dev")
+        self._read_txt(os.path.join(data_dir, "rqe_dev.txt")), "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_csv(os.path.join(data_dir, "rqe_test.csv")), "test")
+        self._read_txt(os.path.join(data_dir, "rqe_test.txt")), "test")
 
   def get_labels(self):
     """See base class."""
